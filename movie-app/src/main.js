a188bc3a5ecfee68cb90e1e15957b7423c55afc6
Doğukan Korkut <doguuu1998@hotmail.com>
2022-03-09 18:32:15 +0300

    Add `vuex`

--- movie-app/src/main.js
+++ movie-app/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
+import store from './store'
 import '../src/assets/styles/main.scss'
 const app = createApp(App);
+app.use(store);
 app.use(router);
 app.mount('#app');
---
+++ movie-app/src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    likedMovies: null
+  },
+  mutations: {
+    setLikeMovies(state, likedMovies) {
+      state.likedMovies = likedMovies;
+    },
+  },
+  getters: {
+    _getLikedMovies: (state) => {
+      return state.likedMovies;
+    },
+  },
+});
